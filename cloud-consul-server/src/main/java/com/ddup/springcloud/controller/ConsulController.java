9ae9e1ebacc4515ecadee4f5c5284483c5ed19dd
haowanjin <2395680185@qq.com>
2020-05-02 01:34:47 +0800

    使用consul作为服务注册中心

---
+++ cloud-consul-server/src/main/java/com/ddup/springcloud/controller/ConsulController.java
@@ -0,0 +1,16 @@
+package com.ddup.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class ConsulController {
+    @GetMapping("get")
+    public String get() {
+        return UUID.randomUUID().toString();
+    }
+}
