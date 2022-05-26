2f925bf0a7763f292cd9272544230ab8e1178f98
haowanjin <239680185@qq.com>
2022-05-26 15:44:28 +0800

    Merge branch 'dev1' of E:\workspace\springcloud2020 with conflicts.

--- spring-quartz-study/src/test/java/com/ddup/quartz/thread/ThreadPoolDemo.java
+++ spring-quartz-study/src/test/java/com/ddup/quartz/thread/ThreadPoolDemo.java
@@ -3,6 +3,12 @@ package com.ddup.quartz.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,9 +17,36 @@ import java.util.concurrent.Executors;
  * @Date 18-07-2020
  */
 public class ThreadPoolDemo {
+    private static final String KEY = "QWERTYUIOPLKJHGF";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
         ExecutorService service = Executors.newSingleThreadExecutor();
         service.submit(() -> System.out.println("新年好!"));
+        System.out.println(aes("abc"));
+        System.out.println(des(aes("abc")));
     }
+
+
+    private static String aes(String pwd) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        SecretKey secretKey = new SecretKeySpec(KEY.getBytes(), "AES");//恢复密钥
+        Cipher cipher = Cipher.getInstance("AES");//Cipher完成加密或解密工作类
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);//对Cipher初始化，解密模式
+        byte[] cipherByte = cipher.doFinal(pwd.getBytes());//加密data
+
+        return Base64.getEncoder().encodeToString(cipherByte);
+    }
+
+    private static String des(String pwd) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        System.out.println(pwd);
+
+        SecretKey secretKey = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), "AES");//恢复密钥
+        Cipher cipher = Cipher.getInstance("AES");//Cipher完成加密或解密工作类
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);//对Cipher初始化，解密模式
+
+        byte[] encrypted1 = Base64.getDecoder().decode(pwd);//先用base64解密
+        byte[] original = cipher.doFinal(encrypted1);
+
+        return new String(original, StandardCharsets.UTF_8);
+    }
+
 }
